3698f09794d3a18a46bf62e6aa064b7400ddd107
Esperanza <esperanza.llontop27@gmail.com>
2024-10-15 09:03:14 +0200

    Aggiungere i file di progetto.

---
+++ Academy2024Exercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+
+/*creare un programma che prenda in input dall'utente i due lati di un triangolo rettangolo
+e che vada a calcolare l'ipotenusa tramite il teorema di pitagora:
+ipotenusa = sqrt(lato1*lato1 + lato2*lato2).
+Stampare il risultato all'utente.
+*/
+
+
+Console.Write("Insert base side length: ");
+string baseSideInput = Console.ReadLine();
+double baseSide = double.Parse(baseSideInput);
+// controlli di guardia 
+if (baseSide < 0 || baseSide > 20)
+{
+    Console.WriteLine("YOU INSERTED AN INVALID BASE SIDE");
+    return;
+}
+
+Console.Write("Insert height side length: ");
+string heightSideInput = Console.ReadLine();
+double heightSide = double.Parse(heightSideInput);
+if (heightSide < 0)
+{
+    Console.WriteLine("YOU INSERTED A BASE SIDE");
+    return;
+}
+
+double hypotenuse = Math.Sqrt(Math.Pow(baseSide, 2) + Math.Pow(heightSide, 2));
+
+Console.WriteLine($"Hypotenuse:  {hypotenuse} cm");
+Console.WriteLine();
+
+
+
+
+
+
---
+++ Academy2024ExerciseConditional/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Collections.Generic;
+
+/*  creare un programma nel quale vengono presi in input due numeri dall'utente,
+dopodiché dire all'utente (tramite Console.Write o WrileLine) quale tra i due è il maggiore o se sono uguali
+*/
+
+int firstNumber;
+bool isNum1Valid;
+
+do
+{
+    Console.Write("Insert first number: ");
+    isNum1Valid = int.TryParse(Console.ReadLine(), out firstNumber);
+    if (!isNum1Valid)
+    {
+        Console.WriteLine("the number invalid");
+    }
+}
+while (!isNum1Valid);
+
+int secondNumber;
+bool isNum2Valid;
+
+do
+{
+    Console.Write("Insert second number: ");
+    string secondNumberInput = Console.ReadLine();
+    isNum2Valid = int.TryParse(secondNumberInput, out secondNumber);
+    if (!isNum2Valid)
+    {
+        Console.WriteLine("the number invalid");
+    }
+}
+while (!isNum2Valid);
+
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine($"The first number {firstNumber} is greater than the second number");
+}
+else if (secondNumber > firstNumber)
+{
+    Console.WriteLine($"The second number {secondNumber} is greater than the first number");
+}
+else
+{
+    Console.WriteLine($"The first number {firstNumber} is the same as the second number");
+}
